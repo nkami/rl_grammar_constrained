b5485911e2fe0ed6de1ff623edc5ac7c1fc950ba
nimi <nimrod.barazani@gmail.com>
2020-01-03 19:55:52 +0200

    grammar filter + grammar file naming for convenience

--- action_filters.py
+++ action_filters.py
@@ -1,4 +1,4 @@
-cyk_parser = __import__("cyk-prefix-parser.CYK_Paser")
+from cyk_prefix_parser.CYK_Paser import Grammar
 
 
 class ActionFilter:
@@ -31,3 +31,24 @@ class AllPassFilter(ActionFilter):
         return True
 
 
+class GrammarFilter(ActionFilter):
+
+    MAX_HISTORY = 2
+
+    def __init__(self):
+        super().__init__(self)
+        self.cyk_prefix = Grammar("grammar.txt")
+
+    def __call__(self, *args, **kwargs):
+        """
+        :param args: an action
+        :param kwargs:
+        :return: returns a bool: True if the action is legal, else False.
+        """
+        string = ' '.join(self.past_actions) + " " + args[0]
+        return self.cyk_prefix.parse(string)
+
+    def add_action(self, action):
+        self.past_actions.append(action)
+        if len(self.past_actions) >= self.MAX_HISTORY:
+            self.reset_past()
