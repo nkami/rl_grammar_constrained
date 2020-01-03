007fe4b46253fe2ad62e9a568ec2d01855dcee2b
nbarazani <nimrod.barazani@gmail.com>
2020-01-03 22:03:39 +0200

    minigrid + bug fix for grammar filter

--- action_filters.py
+++ action_filters.py
@@ -36,7 +36,7 @@ class GrammarFilter(ActionFilter):
     MAX_HISTORY = 2
 
     def __init__(self):
-        super().__init__(self)
+        super().__init__()
         self.cyk_prefix = Grammar("grammar.txt")
 
     def __call__(self, *args, **kwargs):
@@ -45,10 +45,10 @@ class GrammarFilter(ActionFilter):
         :param kwargs:
         :return: returns a bool: True if the action is legal, else False.
         """
-        string = ' '.join(self.past_actions) + " " + args[0]
+        string = ' '.join(self.past_actions) + " " + str(args[0])
         return self.cyk_prefix.parse(string)
 
     def add_action(self, action):
-        self.past_actions.append(action)
+        self.past_actions.append(str(action))
         if len(self.past_actions) >= self.MAX_HISTORY:
             self.reset_past()
--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from stable_baselines_master.stable_baselines.deepq.dqn import DQN
+from stable_baselines_master.stable_baselines import DQN
 import gym
-from action_filters import AllPassFilter
+import gym_minigrid
+from gym_minigrid.wrappers import *
+from action_filters import GrammarFilter, AllPassFilter
 
 
 if __name__ == '__main__':
     time_steps = 100000
-    env_name = "LunarLander-v2"
+
+    env_name = 'MiniGrid-FourRooms-v0'
     env = gym.make(env_name)
+    env = RGBImgPartialObsWrapper(env)
+    env = ImgObsWrapper(env)
+
     log_dir = "./log/"
 
     model = DQN('MlpPolicy', env, verbose=1, tensorboard_log=log_dir, filter=AllPassFilter())
-    model.learn(total_timesteps=time_steps, tb_log_name="LunarLander-v2 : Action Repetition using Grammar")
+    model.learn(total_timesteps=time_steps, tb_log_name="MiniGrid-FourRooms-v0 : AllPassFilter")
 
 
